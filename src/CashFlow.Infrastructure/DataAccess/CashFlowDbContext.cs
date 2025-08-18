6a302bf77288a8d005eff89a5c8ad3b4dd83242d
Luiz-F3lipe <luizf3lipe.dev@gmail.com>
2025-08-17 21:35:16 -0300

    create dbcontext

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,16 @@
+using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string connectionString = "Server=localhost;Database=cashflow;Uid=root;Pwd=password";
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 43));
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
