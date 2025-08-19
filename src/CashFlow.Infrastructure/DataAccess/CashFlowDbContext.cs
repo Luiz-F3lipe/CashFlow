2f22793fa78e6ca82932d3046102badf4ae134d2
LuizF3lipe <luizf3lipe.dev@gmail.com>
2025-08-19 08:39:51 -0300

    using appsettings to read the connection string

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,12 +5,8 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
     public DbSet<Expense> Expenses { get; set; }
     
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        const string connectionString = "Server=127.0.0.1;Database=cashflow;Uid=root;Pwd=password";
-        var serverVersion = new MySqlServerVersion(new Version(8, 0, 43));
-        optionsBuilder.UseMySql(connectionString, serverVersion);
-    }
 }
